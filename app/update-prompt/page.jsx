71f66a8931820c3648a981843fe6204202659b74
Gong Moonphruk <gong.moonphruk@gmail.com>
2024-05-01 20:15:27 +0200

    Refactor update-prompt/page.jsx to remove Suspense and simplify Form component usage

--- app/update-prompt/page.jsx
+++ app/update-prompt/page.jsx
@@ -2,8 +2,6 @@
 
 import { useEffect, useState } from "react";
 import { useSearchParams, useRouter } from "next/navigation";
-import { Suspense } from 'react'
-
 
 import Form from "@components/Form";
 
@@ -57,15 +55,13 @@ const EditPrompt = () => {
     }
 
     return (
-        <Suspense>
-            <Form 
-                type="Edit" 
-                post={post}
-                setPost={setPost}
-                submitting={submitting}
-                handleSubmit={updatePrompt}
-            />
-        </Suspense>
+        <Form 
+            type="Edit" 
+            post={post}
+            setPost={setPost}
+            submitting={submitting}
+            handleSubmit={updatePrompt}
+        />
     )
 }
 
